f554a06f70bfc5f19dcc1bd4da4d3d9e639f4e89
Sameer Paudel <paudelsameer888@gmail.com>
2024-11-06 15:12:11 +0545

    Fixed bugs

--- betterTODOapp/src/App.jsx
+++ betterTODOapp/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { TodoProvider } from "./Contexts";
 import { TodoForm } from "./Components";
-import {TodoItem} from "./Components";
+import { TodoItem } from "./Components";
+import "./font.css";
+import Time from "./Components/Time";
+import DateToday from "./Components/Date";
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -13,48 +16,57 @@ function App() {
     setTodos((prev) => prev.map((t) => (t.id === id ? todo : t)));
   };
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((t)=> (t.id!==id)))
+    setTodos((prev) => prev.filter((t) => t.id !== id));
   };
 
   const toggleChecked = (id) => {
-    setTodos((prev)=> prev.map((t)=> (t.id===id ? {...t, checked: !t.checked} : t)))
+    setTodos((prev) =>
+      prev.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
   };
-
 
   useEffect(() => {
     const todoFromLocalstorage = JSON.parse(localStorage.getItem("todos"));
     if (todoFromLocalstorage && todoFromLocalstorage.length) {
       setTodos(todoFromLocalstorage);
     }
-  }, [])
-
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-  
 
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleChecked }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Todo</h1>
+      <div className="bg-[#e13e94] min-h-screen ">
+        <div className="flex flex-row text-white  justify-between w-full mb-8 pt-5">
+          <Time />
+
+          <h1 className="text-3xl font-bold w-1/3 text-center ">Todo</h1>
+
+          <DateToday />
+        </div>
+        <div className="w-full max-w-2xl mx-auto  rounded-lg px-4 py-3 text-white ">
           <div className="mb-4">
-
-    <TodoForm />
-
+            <TodoForm />
           </div>
-          <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
-            {/* <TodoItem  /> */}
-            {
-              todos.map((todo) => 
-              <div className="w-full" key={todo.id}>
-                <TodoItem todo={todo} />
-              </div>)
-            }
+          <div className="flex flex-wrap gap-y-3   p-3  rounded-lg  border-2 border-[#344455]  ">
+            {todos.length > 0 ? (
+              todos.map((todo) => (
+                <div
+                  className="w-full shadow-md shadow-black rounded-lg "
+                  key={todo.id}
+                >
+                  <TodoItem todo={todo} />
+                </div>
+              ))
+            ) : (
+              <div className="text-xl w-full text-center  text-black">
+                Empty list
+              </div>
+            )}
           </div>
         </div>
       </div>
